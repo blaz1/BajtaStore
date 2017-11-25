5a27336fbe3c220455a015f1bdc2621621f4bf40
blaz1 <artac.blaz@gmail.com>
2017-11-26 00:42:30 +0100

    removed extra wrapper class for sql commands, because hard to pass params to prevent sql injection

--- Server/model.py
+++ Server/model.py
@@ -1,10 +1,15 @@
-from mysql_commands import *
+import sqlalchemy
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:bajtastore@127.0.0.1/mydb')
+Session = scoped_session(sessionmaker(bind=engine))
 
 def select_all_apps():
-	return select_row_from_mysql_command("SELECT * FROM apps")
+	return s.execute("SELECT * FROM apps").fetchall()
 
 def select_all_apps_from_user(user_id):
-	return select_row_from_mysql_command("SELECT * FROM apps a LEFT JOIN users_apps ua ON ua.app_id = a.id WHERE ua.user_id=?", user_id)
+	return s.execute("SELECT * FROM apps a LEFT JOIN users_apps ua ON ua.app_id = a.id WHERE ua.user_id=?", user_id)
 
 def select_all_devices_from_user(user_id):
-	return select_row_from_mysql_command("SELECT * FROM devices WHERE user_id=?", user_id)
+	return s.execute("SELECT * FROM devices WHERE user_id=?", user_id)
