30313ef8b7fd957fc66bcfdc93b1c1f938cbd4aa
blaz1 <artac.blaz@gmail.com>
2017-11-26 00:59:17 +0100

    Revert "fix"
    This reverts commit e356369ccf682682bf1f5745b6792b251b94c426.

--- Server/app.py
+++ Server/app.py
@@ -12,7 +12,7 @@ def store():
 	return render_template('store.html')
 
 @app.route('/apps/<user_id>')
-def apps(user_id):
+def store(user_id):
 	apps_list=select_all_apps_from_user(user_id)
 	return render_template('apps.html', apps=apps_list)
 
