8076b9475ed007a4d0e5d10c9b6938f72f5d35a5
blaz1 <artac.blaz@gmail.com>
2017-11-26 00:59:15 +0100

    Revert "removed extra wrapper class for sql commands, because hard to pass params to prevent sql injection"
    This reverts commit 5a27336fbe3c220455a015f1bdc2621621f4bf40.

--- Server/model.py
+++ Server/model.py
@@ -1,15 +1,10 @@
-import sqlalchemy
-from sqlalchemy import text
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-engine = sqlalchemy.create_engine('mysql+pymysql://root:bajtastore@127.0.0.1/mydb')
-Session = scoped_session(sessionmaker(bind=engine))
+from mysql_commands import *
 
 def select_all_apps():
-	return s.execute("SELECT * FROM apps").fetchall()
+	return select_row_from_mysql_command("SELECT * FROM apps")
 
 def select_all_apps_from_user(user_id):
-	return s.execute("SELECT * FROM apps a LEFT JOIN users_apps ua ON ua.app_id = a.id WHERE ua.user_id=?", user_id)
+	return select_row_from_mysql_command("SELECT * FROM apps a LEFT JOIN users_apps ua ON ua.app_id = a.id WHERE ua.user_id=?", user_id)
 
 def select_all_devices_from_user(user_id):
-	return s.execute("SELECT * FROM devices WHERE user_id=?", user_id)
+	return select_row_from_mysql_command("SELECT * FROM devices WHERE user_id=?", user_id)
