2e5615e2d14988129dcbeb01aacf83940f6330ec
blaz1 <artac.blaz@gmail.com>
2017-11-25 23:23:14 +0100

    home page displays users apps. db create script updated with foreign keys and many to many relationship between users and apps.

--- Server/app.py
+++ Server/app.py
@@ -4,13 +4,19 @@ from model import *
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/<user_id>')
+def home(user_id):
+	if (user_id != NaN)
+		apps_list=select_all_apps_from_user(user_id)
+	else
+		apps_list=NaN
+    return render_template('home.html', apps=apps_list)
 
 @app.route('/store')
 def store():
     return render_template('store.html')
 
+@app.route('/devices/')
 @app.route('/devices/<user_id>')
 def devices(user_id):
     devices_list=select_all_devices_from_user(user_id)
--- Server/model.py
+++ Server/model.py
@@ -9,6 +9,13 @@ def select_all_apps():
 	rows = cur.fetchall()
 	return rows
 
+def select_all_apps_from_user(user_id):
+	cur = db_conn.cursor(user_id)
+	cur.execute("SELECT * FROM apps a LEFT JOIN users_apps ua ON ua.app_id = a.id WHERE ua.user_id=?", user_id)
+
+	rows = cur.fetchall()
+	return rows
+
 def select_all_devices_from_user(user_id):
 	cur = db_conn.cursor()
 	cur.execute("SELECT * FROM devices WHERE user_id=?", user_id)
