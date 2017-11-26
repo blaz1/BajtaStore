2ae04ff7c3fdff71d6af86c6b6bd0a65ac26d33b
blaz1 <artac.blaz@gmail.com>
2017-11-26 02:38:31 +0100

    testing insert

--- Server/model.py
+++ Server/model.py
@@ -26,6 +26,7 @@ def select_all_devices_from_user(user_id):
 def insert_device_data(device_id, json_data):
 	cur = db_conn.cursor()
 	new_device_data_id = len(cur.execute("SELECT * FROM device_data").fetchall()) + 1
+	cur = db_conn.cursor()
 	print("new_device_data_id: ", new_device_data_id)
 	cur.execute("INSERT INTO device_data(id, data, device_id) VALUES (?, ?, ?)", (new_device_data_id, json_data, device_id))
 	#cur.commit()
